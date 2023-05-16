2a994edf0d77ffff1f7a83a590cb38f7d581aff1
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 15:21:54 +0300

    Update BodyMassIndex.java
    explainer lines added

--- JAVA101/AllJavaProjects/src/basicJAVA/BodyMassIndex.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/BodyMassIndex.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 
 public class BodyMassIndex {
     public static void main(String[] args){
+        // height ve weight değişkenlerinin türlerini belirttik.
         float boy, kilo;
-
+        
+        // Scanner sınıfımızı tanımdalık.
         Scanner input = new Scanner(System.in);
-
+        
+        //  Kullanıcıdan verileri istedik.
         System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz : ");
         boy = input.nextFloat();
 
         System.out.println("Lütfen kilonuzu giriniz : ");
         kilo = input.nextFloat();
 
+        // Vücut kitle endeksini hesaplayıp yazdırdık.
         System.out.println("Vücut kitle endeksiniz: " + kilo/boy*boy);
        
-
         
      }
 }
