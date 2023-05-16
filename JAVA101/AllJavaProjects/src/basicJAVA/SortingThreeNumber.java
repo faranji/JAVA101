92428ad8b95fbbb5600ecc230c825b57d62a807f
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 16:34:44 +0300

    added explainer lines

--- JAVA101/AllJavaProjects/src/basicJAVA/SortingThreeNumber.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/SortingThreeNumber.java
@@ -2,11 +2,13 @@ package basicJAVA;
 import java.util.Scanner;
 public class SortingThreeNumber {
     public static void main (String[] args){
+        // Değişkenlerimizin türlerini belirttik.
         int num1, num2, num3;
-        int biggest, big, smallest;
 
+        // Scanner sınıfımızı tanımdalık.
         Scanner input = new Scanner (System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("İlk Sayıyı Giriniz: ");
         num1 = input.nextInt();
         System.out.print("İkinci Sayıyı Giriniz: ");
@@ -14,6 +16,7 @@ public class SortingThreeNumber {
         System.out.print("Üçüncü Sayıyı Giriniz: ");
         num3 = input.nextInt();
 
+        // sayıların sıralanma ihtimallerine göre if-else blokları kullanıp sıralamayı yazdırdık.
         if(num1 > num2 && num1 > num3){
             if(num2>num3){
                 System.out.println( num1 + ">" + num2 + ">" + num3);
