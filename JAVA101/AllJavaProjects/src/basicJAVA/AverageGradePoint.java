6df7d69f876e0b03bdf00dfe5e9a907bb3065061
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 15:31:15 +0300

    Update AverageGradePoint.java
    explainer lines added

--- JAVA101/AllJavaProjects/src/basicJAVA/AverageGradePoint.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/AverageGradePoint.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class AverageGradePoint {
     public static void main(String[] args){
         
+        // Değişkenlerimizin türlerini belirttik.
         int math, physics, chemistry, turkish, history, music;
+        
+        // Scanner sınıfımızı tanımdalık.
         Scanner inp = new Scanner(System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("Matematik Notunuz: ");
         math = inp.nextInt();
 
@@ -24,11 +28,16 @@ public class AverageGradePoint {
 
         System.out.print("Müzik Notunuz: ");
         music = inp.nextInt();
-
+        
+        // Ders notlarının toplamını "sum" adlı değişkene atadık.
         int sum = (math + physics + chemistry + turkish + history + music);
+        // sum adlı değişkeni ders sayısına bölerek ortalamayı elde ettik ve onu result adlı değişkene atadık.
         double result = sum/6.0;
 
+        // Ortalamayı atadığımız değişkeni yazdırdık.
         System.out.println("Ortalamanız : " + result);
-        System.out.println((result > 60) ? "Geçti" : "Geçmedi"); //result değişkenimiz yani ortalamamız 60'tan büyükse Geçti değilse Geçmedi yazar ekrana.
+        
+        //Ortalama 60'tan büyükse 'Geçti' değilse 'Geçmedi' yazdırdık.
+        System.out.println((result > 60) ? "Geçti" : "Geçmedi");
     }
 }
