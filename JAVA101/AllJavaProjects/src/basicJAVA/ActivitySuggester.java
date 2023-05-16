1aefcfef11ce8dc26a1c1b92ea42f7d2a3fa9244
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 16:32:08 +0300

    added explainer lines

--- JAVA101/AllJavaProjects/src/basicJAVA/ActivitySuggester.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/ActivitySuggester.java
@@ -2,13 +2,17 @@ package basicJAVA;
 import java.util.Scanner;
 public class ActivitySuggester {
     public static void main(String[] args){
-
+        // Değişkenlerimizin türlerini belirttik.
         int heat;
+        
+        // Scanner sınıfımızı tanımdalık.
         Scanner input = new Scanner(System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("Sıcaklık Giriniz : ");
         heat = input.nextInt();
 
+        // Sıcaklık 5'ten küçükse, 5-15 aralığındaysa, 10-25 aralığındaysa ve/veya 25'ten büyükse önerilecek aktiviteler yazdırıldı.
         if(heat<5){
             System.out.println("Kayak Yapabilirsiniz.");
         }
