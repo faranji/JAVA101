4d4eb4091b3c69e071da55b5d9a6a509945c9698
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 16:28:46 +0300

    added explainer lines

--- JAVA101/AllJavaProjects/src/basicJAVA/LoginController.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/LoginController.java
@@ -2,16 +2,20 @@ package basicJAVA;
 import java.util.Scanner;
 public class LoginController {
     public static void main (String[] args){
+        // Değişkenlerimizin türlerini belirttik.
         String userName, password;
 
+        // Scanner sınıfımızı tanımdalık.
         Scanner input = new Scanner(System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("Kullanıcı Adınız: ");
         userName = input.nextLine();
 
         System.out.print("Şifreniz: ");
         password = input.nextLine();
 
+        // kullanıcı adı ve şifre doğruysa Giriş Yaptınız ! değilse Bilgileriniz Yanlış ! yazdırır.
         if(userName.equals("Simon") && password.equals("quoetyu00")){
             System.out.println("Giriş Yaptınız !");
         }else{
