adf01bdb07c2b733c6a8d26da741ddde61e97bc5
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 16:44:18 +0300

    added explainer lines

--- JAVA101/AllJavaProjects/src/basicJAVA/LeapYearCalculator.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/LeapYearCalculator.java
@@ -2,12 +2,17 @@ package basicJAVA;
 import java.util.Scanner;
 public class LeapYearCalculator {
     public static void main(String[] args){
+        // Değişkenlerimizin türlerini belirttik.
         int year;
+        
+        // Scanner sınıfımızı tanımdalık.
         Scanner input = new Scanner(System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("Yıl Giriniz : ");
         year = input.nextInt();
 
+        // Yıl 4'e bölünüyorsa 100'e bölünüyor mu diye bakarız. Bölünmüyorsa artık yıldır bölünüyorsa 400'e bölünüyor mu diye bakılır, bölünüyorsa artık yıldır bölünmüyorsa değildir.
         System.out.println((year%4==0)? ((year%100==0)? ((year%400==0)? ((year + " bir artık yıldır !")):((year + " bir artık yıl değildir !"))):(year + " bir artık yıldır !")):(year + " bir artık yıl değildir !"));
     }
 }
