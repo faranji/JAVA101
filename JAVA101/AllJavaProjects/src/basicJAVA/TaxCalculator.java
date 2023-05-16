b18bdebc43a14676ca3b59018d97075677d4d7bc
faranji <112563989+faranji@users.noreply.github.com>
2023-05-16 15:41:18 +0300

    explainer lines added

--- JAVA101/AllJavaProjects/src/basicJAVA/TaxCalculator.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/TaxCalculator.java
@@ -4,24 +4,32 @@ import java.util.Scanner;
 public class TaxCalculator {
     public static void main(String[] args){
     
+    // Değişkenlerimizin türlerini belirttik.
     int moneyValue; 
     double taxPercent= 1.0;
+        
+    // Scanner sınıfımızı tanımdalık.    
     Scanner input = new Scanner(System.in);
 
+    //  Kullanıcıdan verileri istedik.
     System.out.print("KDV'li fiyatını öğrenmek istediğiniz para değerini giriniz:  ");
     moneyValue = input.nextInt();
-
+        
+   // Para değeri 0-1000 aralığındaysa vergi yüzdesine 0.18, 1000'den büyükse 0.08 değeri atanacak.
     if (0 <= moneyValue && moneyValue < 1000){
           taxPercent = 0.18;
     }else if(moneyValue>=1000){
           taxPercent = 0.08;
     }
 
+    // Vergiyle beraber toplam para değerini priceWithTax adlı değişkene atadık.
     double priceWithTax = ((double)moneyValue * taxPercent) + (double)moneyValue;
+    // Vergi miktarını taxValue adlı değişkene atadık.
     double taxValue = (priceWithTax - (double)moneyValue);
 
-        System.out.println("KDV'li ücret: " + priceWithTax + "TL");
-        System.out.println("Vergi Tutarı: " + taxValue + "TL");
+    // KDV'li ücreti ve vergi tutarını yazdırdık.
+    System.out.println("KDV'li ücret: " + priceWithTax + "TL");
+    System.out.println("Vergi Tutarı: " + taxValue + "TL");
 
     }
 }
