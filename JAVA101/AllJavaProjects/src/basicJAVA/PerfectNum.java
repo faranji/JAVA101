0d8a31c9126677bd62968a167f9e5dd947f89177
faranji <112563989+faranji@users.noreply.github.com>
2023-05-18 20:39:51 +0300

    explainer lines added

--- JAVA101/AllJavaProjects/src/basicJAVA/PerfectNum.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/PerfectNum.java
@@ -2,10 +2,13 @@ package basicJAVA;
 import java.util.Scanner;
 public class PerfectNum {
     public static void main(String[] args){
+        // Değişkenlerimizin türlerini belirttik.
         int perfectNumber =0, toplam =0;
-//YANLIŞLIK OLABİLİR!! DİKKAT 0 VE 1 DEĞERLERİ DOĞRU MU BİLMİYORUM.
+
+        // Scanner sınıfımızı tanımdalık.
         Scanner input = new Scanner (System.in);
 
+        // Negatif sayı girilene kadar girilen sayının kendisi hariç bölenlerinin toplamını bulan döngü.
         while(perfectNumber >= 0) {
             System.out.print("Bir sayı giriniz: ");
             perfectNumber = input.nextInt();
@@ -13,14 +16,14 @@ public class PerfectNum {
             if (perfectNumber < 0) {
                 break; // Exit the loop if the number is negative
             }
-
             for (int i = 1; i < perfectNumber; i++) {
                 if (perfectNumber % i == 0) {
                     toplam += i;
                 }
             }
-
+            
             System.out.print(perfectNumber + " ");
+            // Kendisi hariç çarpanları toplamı, kendisine eşitse "Mükemmel sayıdır" değilse "Mükemmel sayı değildir" yazdırır.
             System.out.println((perfectNumber == toplam) ? "Mükemmel sayıdır." : "Mükemmel sayı değildir.");
         }
 
