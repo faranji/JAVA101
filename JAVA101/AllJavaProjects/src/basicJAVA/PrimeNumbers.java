fece5b3651012c78e2fc2b43a7bb5d9dbd52ea16
faranji <112563989+faranji@users.noreply.github.com>
2023-05-18 20:57:06 +0300

    Add files via upload

--- JAVA101/AllJavaProjects/src/basicJAVA/PrimeNumbers.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/PrimeNumbers.java
@@ -1,11 +1,20 @@
 package basicJAVA;
-import java.util.Scanner;
 
 public class PrimeNumbers {
     public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
-
+        for(int i=2; i<=100;i++){
+            int control = 1;
+            for(int j=2; j<=i; j++){
+                if(i%j==0 && j!=i){
+                    control = 5;
+                     break;
+                }
+            }
+            if(control == 1){
+                System.out.print(i + " ");
+            }
+        }
 
     }
 }
