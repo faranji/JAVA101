36f4a7a9455a1b1418b918caaf41381696af4d9c
faranji <112563989+faranji@users.noreply.github.com>
2023-05-20 12:19:30 +0300

    explainer lines added

--- JAVA101/AllJavaProjects/src/basicJAVA/ReverseTriangleWithStars.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/ReverseTriangleWithStars.java
@@ -2,14 +2,17 @@ package basicJAVA;
 import java.util.Scanner;
 public class ReverseTriangleWithStars {
     public static void main(String[] args){
+        // Değişkenlerimizin türlerini belirttik.
         int rowNumber;
+        
+        // Değişkenlerimizin türlerini belirttik.
         Scanner input = new Scanner(System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("Satır sayısını giriniz: ");
         rowNumber = input.nextInt();
 
-
-        // tekrar kendin yapmaya çalış
+        // Ters piramit yazdıran döngü.
         for(int i=1; i<=rowNumber; i++){
             for(int k=1; k<=i-1; k++){
                 System.out.print(" ");
