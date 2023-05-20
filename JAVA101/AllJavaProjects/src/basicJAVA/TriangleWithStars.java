3b171393b47ad09a32169ac518cb7ef3307cb139
faranji <112563989+faranji@users.noreply.github.com>
2023-05-20 12:23:45 +0300

    explainer lines added

--- JAVA101/AllJavaProjects/src/basicJAVA/TriangleWithStars.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/TriangleWithStars.java
@@ -2,11 +2,27 @@ package basicJAVA;
 import java.util.Scanner;
 public class TriangleWithStars {
     public static void main(String[] args){
+        // Değişkenlerimizin türlerini belirttik.
         int rowNumber;
+        
+        // Değişkenlerimizin türlerini belirttik.
         Scanner input = new Scanner(System.in);
 
+        //  Kullanıcıdan verileri istedik.
         System.out.print("Kaç satırlık üçgen istiyorsun?");
         rowNumber = input.nextInt();
+        
+        
+        for (int i = 1; i <= rowNumber; i++){
+            for(int k= rowNumber-i; k>=0; k--){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
     }
