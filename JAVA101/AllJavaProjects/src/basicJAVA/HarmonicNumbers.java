8c5ececaf4f3f1a2a1a320d50382b20dd7e91b69
faranji <112563989+faranji@users.noreply.github.com>
2023-05-22 22:09:56 +0300

    final touches

--- JAVA101/AllJavaProjects/src/basicJAVA/HarmonicNumbers.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/HarmonicNumbers.java
@@ -2,7 +2,18 @@ package basicJAVA;
 import java.util.Scanner;
 public class HarmonicNumbers {
     public static void main(String[] args){
-
+        int n;
+        double sum=0.0;
+        
         Scanner input = new Scanner(System.in);
+        
+        System.out.print("N sayısını giriniz: ");
+        n = input.nextInt();
+
+        for(int i=1; i<=n;i++){
+            sum += (double)1/i;
+        }
+
+        System.out.println("Sonuç :" + sum);
     }
 }
