1e5d934d99f89d5cea3a4767f1a2ec62dcc262e9
faranji <112563989+faranji@users.noreply.github.com>
2023-06-20 16:53:00 +0300

    Add files via upload

--- JAVA101/AllJavaProjects/src/basicJAVA/ArmstrongNumbers.java
+++ JAVA101/AllJavaProjects/src/basicJAVA/ArmstrongNumbers.java
@@ -2,7 +2,33 @@ package basicJAVA;
 import java.util.Scanner;
 public class ArmstrongNumbers {
     public static void main(String[] args){
+        int sum=0, gir, bolum, basamakSayisi=1;
 
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayi giriniz: ");
+        gir = input.nextInt();
+
+        int originalNumber = gir;
+
+        do {
+            basamakSayisi++;
+            gir /= 10;
+        } while(gir> 10);
+
+        gir = originalNumber;
+
+        for(int i = 1 ; i <= basamakSayisi; i++){
+            int kalan = gir % 10;
+            gir /= 10 ;
+            sum += Math.pow(kalan, basamakSayisi);
+        }
+
+        if(sum == originalNumber){
+            System.out.println(gir + "is an Armstrong number.");
+        }else{
+            System.out.println(gir + "is not an Armstrong number.");
+        }
+
     }
 }
